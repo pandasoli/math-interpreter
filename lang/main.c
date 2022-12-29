4347db699535339a95a40e0db9d7f4b0cc12c217
pandasoli <sadpandasoli@gmail.com>
2022-12-29 15:57:31 -0300

    changed to use the parser

--- lang/main.c
+++ lang/main.c
@@ -1,15 +1,14 @@
 #include <string.h>
 #include "./lexer/main.c"
+#include "./lexer/tokens.c"
+#include "./parser/main.c"
 
 
 void run(char *text) {
   struct Lexer lex = newLexer(text);
-  struct Token tk;
+  struct Parser par = newParser(&lex);
 
-  do {
-    tk = lex.lex(&lex);
-    tk.print(&tk);
-  }
-  while (tk.kind != EOFTk && tk.kind != ErrTk);
+  struct Node ast = par.parse(&par);
+  ast.print(&ast, "");
 }
 

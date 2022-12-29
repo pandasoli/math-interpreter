0129e28a574807c44b6416f96a6f3e0b14064541
pandasoli <sadpandasoli@gmail.com>
2022-12-29 20:53:46 -0300

    added: better errors

--- lang/main.c
+++ lang/main.c
@@ -2,11 +2,13 @@
 #include "./lexer/main.c"
 #include "./lexer/tokens.c"
 #include "./parser/main.c"
+#include "../error.c"
 
 
 void run(char *text) {
-  struct Lexer lex = newLexer(text);
-  struct Parser par = newParser(&lex);
+  struct Err err = newErr();
+  struct Lexer lex = newLexer(text, &err);
+  struct Parser par = newParser(&lex, &err);
 
   struct Node ast = par.parse(&par);
   ast.print(&ast, "");

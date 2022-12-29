5295eb978aa1b726b65fac45738ed4758149894a
pandasoli <sadpandasoli@gmail.com>
2022-12-29 18:19:46 -0300

    added: user control

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 #include "./lang/main.c"
 
 
-int main(void) {
-  char* code = "1 + 2 * 12";
-  run(code);
+int main(int argc, char **argv) {
+  char code[255];
+
+  if (argc > 1) {
+    if (strcmp(argv[1], "test")) {
+      strcpy(code, "1 + 2 * 12");
+      run(code);
+    }
+    else
+       printf("What the fuck you meant with \"%s\"?", argv[1]);
+  }
+  else {
+    while (true) {
+      printf("> ");
+
+      fgets(code, sizeof(code), stdin);
+      sscanf(code, "%[^\n]s", code);
+
+      if (strcmp(code, "\n") == 0) break;
+
+      run(code);
+    }
+  }
 
   return 0;
 }
